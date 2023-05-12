2ded7c8538f5a95b3e1a57a026dd561fea5eae9f
Jhonwi <jwdiaz93@gmail.com>
2023-05-12 18:12:07 -0500

    Carrito de Compras

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+import loader from './components/loader.js'
+import showMenu from './components/showMenu.js' 
+import showCart from './components/showCart.js'
+import products from './components/products.js'
+import getProducts from './helpers/getProducts.js'
+import cart from './components/cart.js'
+
+/*UI Elements*/
+/*Ocultar loader*/
+loader()
+
+/*Mostrar Menu*/
+showMenu()
+
+/*Mostrar Carrito*/
+showCart()
+
+/*End UI Elements*/
+
+
+/*Products*/
+const { db, printProducts } = products(await getProducts())
+
+/*Carrito*/
+cart(db, printProducts)
